b5cf22d5b9ad3aff0f6d2f79d25c5081f48fe85e
Paul Berger <pb10@yahoo.com>
2014-05-15 07:42:18 -0400

    Dice Roll Simulator with results and frequencies
    This simulator rolls and totals the dice a total of 100 times with the
    click of a button and displays the results. There is also a button to
    click to view the frequency of each roll.

---
+++ Dice Simulation/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Dice_Simulation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInstructionsWrite = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Die1label = new System.Windows.Forms.Label();
+            this.Die2label = new System.Windows.Forms.Label();
+            this.lbxDie1 = new System.Windows.Forms.Label();
+            this.lbxDie2 = new System.Windows.Forms.Label();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lbxTotal = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.rtbxResults = new System.Windows.Forms.RichTextBox();
+            this.rtbxFrequency = new System.Windows.Forms.RichTextBox();
+            this.lblResults = new System.Windows.Forms.Label();
+            this.lblFrequency = new System.Windows.Forms.Label();
+            this.btnFrequency = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblInstructionsWrite
+            // 
+            this.lblInstructionsWrite.AutoSize = true;
+            this.lblInstructionsWrite.Location = new System.Drawing.Point(11, 13);
+            this.lblInstructionsWrite.Name = "lblInstructionsWrite";
+            this.lblInstructionsWrite.Size = new System.Drawing.Size(342, 13);
+            this.lblInstructionsWrite.TabIndex = 0;
+            this.lblInstructionsWrite.Text = "Please click WRITE to run the simulation and add the results to the file.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(294, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please click READ to view the simulation results from the file.";
+            // 
+            // Die1label
+            // 
+            this.Die1label.AutoSize = true;
+            this.Die1label.Location = new System.Drawing.Point(69, 74);
+            this.Die1label.Name = "Die1label";
+            this.Die1label.Size = new System.Drawing.Size(35, 13);
+            this.Die1label.TabIndex = 2;
+            this.Die1label.Text = "Die 1:";
+            // 
+            // Die2label
+            // 
+            this.Die2label.AutoSize = true;
+            this.Die2label.Location = new System.Drawing.Point(260, 74);
+            this.Die2label.Name = "Die2label";
+            this.Die2label.Size = new System.Drawing.Size(35, 13);
+            this.Die2label.TabIndex = 3;
+            this.Die2label.Text = "Die 2:";
+            // 
+            // lbxDie1
+            // 
+            this.lbxDie1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbxDie1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxDie1.Location = new System.Drawing.Point(46, 101);
+            this.lbxDie1.Name = "lbxDie1";
+            this.lbxDie1.Size = new System.Drawing.Size(80, 80);
+            this.lbxDie1.TabIndex = 4;
+            this.lbxDie1.Text = "1";
+            this.lbxDie1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbxDie2
+            // 
+            this.lbxDie2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbxDie2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxDie2.Location = new System.Drawing.Point(237, 101);
+            this.lbxDie2.Name = "lbxDie2";
+            this.lbxDie2.Size = new System.Drawing.Size(80, 80);
+            this.lbxDie2.TabIndex = 5;
+            this.lbxDie2.Text = "1";
+            this.lbxDie2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(139, 81);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(87, 35);
+            this.btnWrite.TabIndex = 0;
+            this.btnWrite.Text = "ROLL THE DICE";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(139, 122);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(87, 35);
+            this.btnRead.TabIndex = 1;
+            this.btnRead.Text = "ROLL RESULTS";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(40, 215);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(157, 33);
+            this.lblTotal.TabIndex = 8;
+            this.lblTotal.Text = "Dice Total";
+            // 
+            // lbxTotal
+            // 
+            this.lbxTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbxTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxTotal.Location = new System.Drawing.Point(203, 204);
+            this.lbxTotal.Name = "lbxTotal";
+            this.lbxTotal.Size = new System.Drawing.Size(114, 48);
+            this.lbxTotal.TabIndex = 9;
+            this.lbxTotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(225, 322);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(92, 29);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(226, 282);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(92, 29);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // rtbxResults
+            // 
+            this.rtbxResults.Location = new System.Drawing.Point(38, 282);
+            this.rtbxResults.Name = "rtbxResults";
+            this.rtbxResults.Size = new System.Drawing.Size(64, 69);
+            this.rtbxResults.TabIndex = 12;
+            this.rtbxResults.Text = "";
+            // 
+            // rtbxFrequency
+            // 
+            this.rtbxFrequency.Location = new System.Drawing.Point(108, 282);
+            this.rtbxFrequency.Name = "rtbxFrequency";
+            this.rtbxFrequency.Size = new System.Drawing.Size(112, 69);
+            this.rtbxFrequency.TabIndex = 13;
+            this.rtbxFrequency.Text = "";
+            // 
+            // lblResults
+            // 
+            this.lblResults.AutoSize = true;
+            this.lblResults.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResults.Location = new System.Drawing.Point(38, 263);
+            this.lblResults.Name = "lblResults";
+            this.lblResults.Size = new System.Drawing.Size(64, 13);
+            this.lblResults.TabIndex = 14;
+            this.lblResults.Text = "RESULTS";
+            // 
+            // lblFrequency
+            // 
+            this.lblFrequency.AutoSize = true;
+            this.lblFrequency.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFrequency.Location = new System.Drawing.Point(123, 263);
+            this.lblFrequency.Name = "lblFrequency";
+            this.lblFrequency.Size = new System.Drawing.Size(82, 13);
+            this.lblFrequency.TabIndex = 15;
+            this.lblFrequency.Text = "FREQUENCY";
+            // 
+            // btnFrequency
+            // 
+            this.btnFrequency.Location = new System.Drawing.Point(139, 163);
+            this.btnFrequency.Name = "btnFrequency";
+            this.btnFrequency.Size = new System.Drawing.Size(87, 35);
+            this.btnFrequency.TabIndex = 16;
+            this.btnFrequency.Text = "FREQUENCY OF TOTALS";
+            this.btnFrequency.UseVisualStyleBackColor = true;
+            this.btnFrequency.Click += new System.EventHandler(this.btnFrequency_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 365);
+            this.Controls.Add(this.btnFrequency);
+            this.Controls.Add(this.lblFrequency);
+            this.Controls.Add(this.lblResults);
+            this.Controls.Add(this.rtbxFrequency);
+            this.Controls.Add(this.rtbxResults);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lbxTotal);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.lbxDie2);
+            this.Controls.Add(this.lbxDie1);
+            this.Controls.Add(this.Die2label);
+            this.Controls.Add(this.Die1label);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblInstructionsWrite);
+            this.Name = "Form1";
+            this.Text = "Dice Simulation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstructionsWrite;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Die1label;
+        private System.Windows.Forms.Label Die2label;
+        private System.Windows.Forms.Label lbxDie1;
+        private System.Windows.Forms.Label lbxDie2;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lbxTotal;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.RichTextBox rtbxResults;
+        private System.Windows.Forms.RichTextBox rtbxFrequency;
+        private System.Windows.Forms.Label lblResults;
+        private System.Windows.Forms.Label lblFrequency;
+        private System.Windows.Forms.Button btnFrequency;
+    }
+}
+
---
+++ Dice Simulation/Form1.cs
@@ -0,0 +1,267 @@
+﻿//Paul Berger Week 3 Individual Assignment
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Dice_Simulation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //declare an array
+            int[] ResultsArray;
+
+        //Event handler for exit button
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //closes the program
+            this.Close();
+        }
+
+        
+        //Event handler for the write button
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+
+                //add total to the results file
+                try
+                {
+                    // Declare an output variable
+                    StreamWriter outputResults;
+
+                    // open the file to store to
+                    outputResults = File.CreateText("Results.txt");
+            
+                    //declare a counter to iterate 100 times
+                    int count = 0;
+
+                    //create a random number generator
+                    Random randomNumber = new Random();
+                    
+                    while (count <= 99)
+                    {
+                                               
+                        
+                
+                        //declare variables and pick random die values and total
+                        int Face1 = randomNumber.Next(6) + 1;
+                        int Face2 = randomNumber.Next(6) + 1;
+
+                        //Display the value of each die in the associated label display
+                        lbxDie1.Text = Face1.ToString();
+                        lbxDie2.Text = Face2.ToString();
+
+                        //declare variable and add the dice together
+                        int dieTotal = Face1 + Face2;
+
+                        //display the total in the label display
+                        lbxTotal.Text = dieTotal.ToString();
+
+                        // write to the file
+                        outputResults.WriteLine(dieTotal.ToString());
+
+                        //advance the counter
+                        count = count + 1;
+                    
+                    }
+                    //close the file
+                    outputResults.Close();
+                }
+                catch (Exception ex) //for any errors writing to the file
+                {
+                    MessageBox.Show(ex.Message);
+                }       
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            rtbxResults.Clear();
+            rtbxFrequency.Clear();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            //clear the results box
+            rtbxResults.Clear();
+            
+            //create an array object
+            ResultsArray = new int[100];
+            
+            //try block to verify the file exists
+            try
+            {
+                //declare a variable to hold the dice total
+                string resultTotal;
+
+                //delare a stream reader variable
+                StreamReader inputResults;
+
+                //open the file
+                inputResults = File.OpenText("Results.txt");
+
+                //clear the results list box
+                rtbxResults.Clear();
+
+                //declare a counter
+                int addIndex = 0;
+                
+                //read the contents of the results file and write to the array
+                while (addIndex < 100 && !inputResults.EndOfStream)
+                {
+                    //get the entries
+                    resultTotal = inputResults.ReadLine();
+
+                    //add the elements to the array
+                    ResultsArray[addIndex] = int.Parse(resultTotal);
+                    
+                    //increase index
+                    addIndex ++;
+                    
+                }
+
+                //close the file
+                inputResults.Close();
+
+                //display the elements of the array in the results box
+                foreach (int value in ResultsArray)
+                {
+                    rtbxResults.AppendText(value.ToString() + "\r");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //display an error message
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnFrequency_Click(object sender, EventArgs e)
+        {
+             
+            //clear the frequency box
+            rtbxFrequency.Clear();
+            
+            //declare variables
+            int twos = 0, threes = 0, fours = 0, fives = 0, sixes = 0, sevens = 0, eights = 0, nines = 0, tens = 0, elevens = 0, twelves = 0;
+
+            //read from the array
+            for (int index = 0; index < 100; index ++)
+            {
+                //declare a holder for the array index element
+                int indexValue = ResultsArray[index];
+
+                //determine frequencies
+                if (indexValue == 2)
+                {
+                    twos = twos + 1;
+                }
+
+                else
+                {
+                    if (indexValue == 3)
+                    {
+                        threes = threes + 1;
+                    }
+
+                    else
+                    {
+                        if (indexValue == 4)
+                        {
+                            fours = fours + 1;
+                        }
+
+                        else
+                        {
+                            if (indexValue == 5)
+                            {
+                                fives = fives + 1;
+                            }
+
+                            else
+                            {
+                                if (indexValue == 6)
+                                {
+                                    sixes = sixes + 1;
+                                }
+
+                                else
+                                {
+                                    if (indexValue == 7)
+                                    {
+                                        sevens = sevens + 1;
+                                    }
+
+                                    else
+                                    {
+                                        if (indexValue == 8)
+                                        {
+                                            eights = eights + 1;
+                                        }
+
+                                        else
+                                        {
+                                            if (indexValue == 9)
+                                            {
+                                                nines = nines + 1;
+                                            }
+
+                                            else
+                                            {
+                                                if (indexValue == 10)
+                                                {
+                                                    tens = tens + 1;
+                                                }
+                                                else
+                                                {
+                                                    if (indexValue == 11)
+                                                    {
+                                                        elevens = elevens + 1;
+                                                    }
+
+                                                    else
+                                                    {
+                                                        twelves = twelves + 1;
+
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+                
+            //write elements to frequency box
+                rtbxFrequency.AppendText(twos.ToString() + " Twos rolled." + "\r");
+                rtbxFrequency.AppendText(threes.ToString() + " Threes rolled." + "\r");
+                rtbxFrequency.AppendText(fours.ToString() + " Fours rolled." + "\r");
+                rtbxFrequency.AppendText(fives.ToString() + " Fives rolled." + "\r");
+                rtbxFrequency.AppendText(sixes.ToString() + " Sixes rolled." + "\r");
+                rtbxFrequency.AppendText(sevens.ToString() + " Sevens rolled." + "\r");
+                rtbxFrequency.AppendText(eights.ToString() + " Eights rolled." + "\r");
+                rtbxFrequency.AppendText(nines.ToString() + " Nines rolled." + "\r");
+                rtbxFrequency.AppendText(tens.ToString() + " Tens rolled." + "\r");
+                rtbxFrequency.AppendText(elevens.ToString() + " Elevens rolled." + "\r");
+                rtbxFrequency.AppendText(twelves.ToString() + " Twelves rolled." + "\r");
+                
+            
+        }
+    }
+}
